d9303495dfa003d908dd3c66741548292d75018f
Akhil Nair <4khiln4ir@gmail.com>
2020-07-28 00:00:19 +0530

    Updates

---
+++ precache-manifest.75f706a8dc13afd40b9bd2980f97ca3a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7844e737939fae0f663",
+    "url": "/react-pics/static/css/main.dd0ac617.chunk.css"
+  },
+  {
+    "revision": "c7844e737939fae0f663",
+    "url": "/react-pics/static/js/main.e0edf157.chunk.js"
+  },
+  {
+    "revision": "623226f72673ce2a96e3",
+    "url": "/react-pics/static/js/runtime~main.110b8829.js"
+  },
+  {
+    "revision": "e495c2f9edecd4488c25",
+    "url": "/react-pics/static/js/2.805cbd2c.chunk.js"
+  },
+  {
+    "revision": "a3d5b11b7be9fddb1e5b722ef707741d",
+    "url": "/react-pics/index.html"
+  }
+];
